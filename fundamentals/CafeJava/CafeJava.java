1bcb54c23435035edea91547d8782a15e4543e2a
Lisa <lb.broadhead@gmail.com>
2022-04-02 13:31:09 -0500

    first commit

--- fundamentals/CafeJava/CafeJava.java
+++ fundamentals/CafeJava/CafeJava.java
@@ -21,10 +21,43 @@ public class CafeJava {
     
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = false;
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
-        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
+        
+        // order1
+        if(isReadyOrder1){
+            System.out.println(customer1 + readyMessage + ". " + displayTotalMessage +  dripCoffee );
+
+        }else{
+            System.out.println(generalGreeting + customer1 + ". Great a Coffee" + pendingMessage + "!");
+        }
+
+        System.out.println(generalGreeting + customer4 + "!");
+        // order2
+        if(isReadyOrder2){
+            System.out.println( customer4 +  readyMessage + ". " + displayTotalMessage + cappuccino);
+        }else{
+            System.out.println(customer4 + pendingMessage);
+        } 
+
+        System.out.println(generalGreeting + customer2 + "!");
+        // order3
+        if(isReadyOrder3){
+            System.out.println("")
+        }else{
+        }
+
+        // order4
+        if(isReadyOrder4){
+            System.out.println(generalGreeting + customer4);
+        }else{
+
+        }
     }
 }
