a83abaab7b26b1ac65312fe87ae65235da52489c
Lisa <lb.broadhead@gmail.com>
2022-04-02 13:13:30 -0500

    first commit

--- fundamentals/CafeJava/CafeJava.java
+++ fundamentals/CafeJava/CafeJava.java
@@ -1,8 +1,30 @@
-public class CafeJava{
-    public static void main(String[] args){
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
         double dripCoffee = 5.95;
         double latte = 7.00;
         double cappuccino = 6.32;
-        
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
     }
 }
