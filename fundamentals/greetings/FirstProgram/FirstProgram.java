25c9ddcd35a76e89ec4138e57376e475bc86095f
Lisa <lb.broadhead@gmail.com>
2022-04-02 12:51:31 -0500

    first commit

--- fundamentals/greetings/FirstProgram/FirstProgram.java
+++ fundamentals/greetings/FirstProgram/FirstProgram.java
@@ -1,6 +1,8 @@
-public class FirstProgram{
+public class FirstProgram {
     public static void main(String[] args){
         System.out.println("My name is Coding Dojo");
-        
+        System.out.println("I'm 100 years old");
+        System.out.println("My hometown is Burbank California");
+        System.out.println("How does Conding Dojo know all this about me? It's spot on!");
     }
 }
