5ba437c0062ef4af65d69cbaacbfd75dcd6ed9ac
Lisa <lb.broadhead@gmail.com>
2022-04-02 13:36:32 -0500

    first commit

--- fundamentals/CafeJava/CafeJava.java
+++ fundamentals/CafeJava/CafeJava.java
@@ -22,7 +22,7 @@ public class CafeJava {
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
         boolean isReadyOrder2 = true;
-        boolean isReadyOrder3 = false;
+        boolean isReadyOrder3 = true;
         boolean isReadyOrder4 = false;
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
@@ -30,34 +30,42 @@ public class CafeJava {
         // System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
         
-        // order1
+        // order1 - CINDHURI
         if(isReadyOrder1){
             System.out.println(customer1 + readyMessage + ". " + displayTotalMessage +  dripCoffee );
 
         }else{
             System.out.println(generalGreeting + customer1 + ". Great a Coffee" + pendingMessage + "!");
         }
+        System.out.println();
 
+
+        // order2 - NOAH
         System.out.println(generalGreeting + customer4 + "!");
-        // order2
         if(isReadyOrder2){
             System.out.println( customer4 +  readyMessage + ". " + displayTotalMessage + cappuccino);
         }else{
             System.out.println(customer4 + pendingMessage);
         } 
+        System.out.println();
 
-        System.out.println(generalGreeting + customer2 + "!");
-        // order3
+
+        // order3 - SAM
+        System.out.println(generalGreeting + customer2 + "! My My My, thristy today?");
         if(isReadyOrder3){
-            System.out.println("")
+            System.out.println( customer2 + readyMessage + ". " + displayTotalMessage + latte * 2); 
         }else{
+            System.out.println(customer2 + pendingMessage);
         }
+        System.out.println();
 
-        // order4
+
+
+        // order4 - JIMMY
         if(isReadyOrder4){
             System.out.println(generalGreeting + customer4);
         }else{
-
+            System.out.println(customer4);
         }
     }
 }
