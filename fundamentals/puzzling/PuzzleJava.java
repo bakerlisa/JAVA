901a2bc1bbaf1f9081ae3dfbc4fef029dcd88633
Lisa <lb.broadhead@gmail.com>
2022-04-03 18:58:26 -0500

    first commit

--- fundamentals/puzzling/PuzzleJava.java
+++ fundamentals/puzzling/PuzzleJava.java
@@ -10,10 +10,37 @@ class PuzzleJava {
             Random rand = new Random();
             int upperbound = 20;
             int randInt = rand.nextInt(upperbound); 
-            System.out.println(randInt);
-            // allRolls.add(randInt);
+            allRolls.add(randInt);
             i++;
         }
         return allRolls;
+    }
+
+    public char getRandomLetter(){
+        char randChars;
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    
+        // gets random number
+        Random rand = new Random();
+        int upperbound = 25;
+        int randInt = rand.nextInt(upperbound); 
+        
+        //sets the radnom character to be returned
+        randChars=alphabet[randInt];
+
+        return randChars;
+    }
+
+    public String generatePassword(){
+        String password = new String();
+        int passLength = 8;
+        int i=0;
+        while(i<passLength){
+            password += getRandomLetter();
+            i++;
+        }
+        System.out.println(password);
+
+        return password;
     }
 }
--- fundamentals/puzzling/TestPuzzleJava.java
+++ fundamentals/puzzling/TestPuzzleJava.java
@@ -3,12 +3,18 @@ import java.util.ArrayList;
 public class TestPuzzleJava {
     
 	public static void main(String[] args) {
+		// RANDOM ARRAY OF ROLLS
 		PuzzleJava generator = new PuzzleJava();
 		ArrayList<Integer> randomRolls = generator.getTenRolls();
 		System.out.println(randomRolls);
 		
-    	//..
-		// Write your other test cases here.
-		//..
+    	// RANDOM CHARACTER
+		char newLetter = generator.getRandomLetter();
+		System.out.println(newLetter);
+
+		// Passwordgenerator
+		String newPassword = generator.generatePassword();
+		System.out.println(newPassword);
+
 	}
 }
