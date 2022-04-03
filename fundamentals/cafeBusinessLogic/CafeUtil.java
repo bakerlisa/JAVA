53094f84f69733d4d8e8c25c8068938bd74d9320
Lisa <lb.broadhead@gmail.com>
2022-04-03 15:19:33 -0500

    first commit

--- fundamentals/cafeBusinessLogic/CafeUtil.java
+++ fundamentals/cafeBusinessLogic/CafeUtil.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.text.DecimalFormat;
 
 class CafeUtil{
+    
     public int getStreakGoal(int count){
         int i = 0;
         int sum = 0;
@@ -20,11 +22,56 @@ class CafeUtil{
         
         return total;
     }
-
-    public ArrayList<String> displayMenu(ArrayList<String> menu){
+    
+    public void displayMenu(ArrayList<String> menu){
         for(int i = 0; i < menu.size(); i++){
             System.out.println(i + " " + menu.get(i));
         }
-        return menu;
     }
+    
+    ArrayList<String> myArray = new ArrayList<String>();
+    public void addCustomer(ArrayList<String> customer){
+        System.out.println("Please Print your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello " + userName);
+        System.out.println("There are " + myArray.size() + " people in front of you");
+        myArray.add(userName);
+        System.out.println(myArray);
+    }
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    
+    public void printPriceChart(String product, double price, int maxQuantity){
+        double subtrack = .5;
+        System.out.println(product);
+        for(int i = 1; i< maxQuantity + 1; i++){
+            if(i > 1){
+                double newPrice = (price * i) - subtrack;
+                System.out.println(i + " - $" + df.format(newPrice));
+                subtrack += .5;
+            }else{
+                System.out.println(i + " - $" + df.format(price * i));
+            }
+        }
+    }
+
+    public boolean displayMenu(ArrayList<String> menuItems, ArrayList<Double> prices){
+        
+        if(menuItems.size()  == prices.size()){
+            for(int i=0;i<menuItems.size() ;i++){
+                System.out.println(i + ": "  + menuItems.get(i) + " -- $" + prices.get(i) );
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String addCustomers(){
+        ArrayList<String> customers = new ArrayList<String>();
+        
+        return customers;
+    }
+
 }
+
--- fundamentals/cafeBusinessLogic/TestCafe.java
+++ fundamentals/cafeBusinessLogic/TestCafe.java
@@ -12,6 +12,7 @@ public class TestCafe {
         int numWeeks = 10;
         /* ============ App Test Cases ============= */
     
+        // TEST 1
         System.out.println("\n----- Streak Goal Test -----");
         System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal(numWeeks));
     
@@ -19,8 +20,8 @@ public class TestCafe {
         double[] lineItems = {3.5, 1.5, 4.0, 4.5};
         System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
         
+        // TEST 2
         // System.out.println("----- Display Menu Test-----");
-        
         ArrayList<String> menu = new ArrayList<String>();
         menu.add("drip coffee");
         menu.add("cappuccino");
@@ -28,12 +29,25 @@ public class TestCafe {
         menu.add("mocha");
         appTest.displayMenu(menu);
     
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
-        // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     appTest.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+        // TEST 3
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            // appTest.addCustomer(customers);
+            System.out.println("\n");
+        }
+
+        // BONUS: TEST 4
+        // appTest.printPriceChart("Coffee",2.00,4);
+
+        // BONUS: TEST 5
+        ArrayList<Double> prices = new ArrayList<Double>();
+        prices.add(12.00);
+        prices.add(3.75);
+        prices.add(6.99);
+        prices.add(7.50);
+
+        appTest.displayMenu(menu,prices);
     }
 }
