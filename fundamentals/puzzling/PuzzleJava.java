33fe7d94e14eb74b635c4c443e752ed21e710ddb
Lisa <lb.broadhead@gmail.com>
2022-04-03 18:36:31 -0500

    first commit

--- fundamentals/puzzling/PuzzleJava.java
+++ fundamentals/puzzling/PuzzleJava.java
@@ -1,5 +1,19 @@
-public class PuzzleJava{
-    public static void main(String[] args){
+import java.util.Random;
+import java.util.ArrayList;
 
+class PuzzleJava {
+    public ArrayList<Integer> getTenRolls(){
+        ArrayList<Integer> allRolls = new ArrayList<Integer>();
+        
+        int i=0;
+        while(i<10){
+            Random rand = new Random();
+            int upperbound = 20;
+            int randInt = rand.nextInt(upperbound); 
+            System.out.println(randInt);
+            // allRolls.add(randInt);
+            i++;
+        }
+        return allRolls;
     }
 }
---
+++ fundamentals/puzzling/TestPuzzleJava.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TestPuzzleJava {
+    
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+		
+    	//..
+		// Write your other test cases here.
+		//..
+	}
+}
