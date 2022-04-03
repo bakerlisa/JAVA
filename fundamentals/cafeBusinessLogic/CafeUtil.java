777b6c5a113525f6d054b89338dc925b1a3fb6d6
Lisa <lb.broadhead@gmail.com>
2022-04-03 14:10:58 -0500

    first commit

--- fundamentals/cafeBusinessLogic/CafeUtil.java
+++ fundamentals/cafeBusinessLogic/CafeUtil.java
@@ -1,7 +1,30 @@
-class CafeUtil{
-    int numWeeks = 10;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+class CafeUtil{
     public int getStreakGoal(int count){
-        return count;
+        int i = 0;
+        int sum = 0;
+        while(i < count){
+            sum = sum + (i + 1);
+            i++;
+        }
+        return sum;
+    }
+
+    public double getOrderTotal(double[] prices){
+        double total = 0.0;
+        for(int i = 0; i <prices.length; i++){
+            total += prices[i];
+        }
+        
+        return total;
+    }
+
+    public ArrayList<String> displayMenu(ArrayList<String> menu){
+        for(int i = 0; i < menu.size(); i++){
+            System.out.println(i + " " + menu.get(i));
+        }
+        return menu;
     }
 }
