5da28a537b1d5793e5cf747b5bee1180501841d7
Lisa <lb.broadhead@gmail.com>
2022-04-04 15:57:34 -0500

    first commit

--- oop/orderItems/TestOrders.java
+++ oop/orderItems/TestOrders.java
@@ -17,26 +17,26 @@ public class TestOrders {
 
         Order order1 = new Order();  
         order1.name = "Cindhuri";
-        order1.cafe = order1.getCafe(Item1.get(0));
-        order1.total = order1.getPrice(Item1.get(1));
+        order1.cafe = order1.typeCastCafe(Item1.get(0));
+        order1.total = order1.typeCastPrice(Item1.get(1));
         order1.ready = true;
 
         Order order2 = new Order();  
         order2.name = "Jimmy";
-        order2.cafe = order2.getCafe(Item2.get(0));
-        order2.total = order2.getPrice(Item2.get(1));
+        order2.cafe = order2.typeCastCafe(Item3.get(0));
+        order2.total = order2.typeCastPrice(Item3.get(1));
         order2.ready = true;
 
         Order order3 = new Order();  
         order3.name = "Noah";
-        order3.cafe = order3.getCafe(Item4.get(0)) + ", " + order3.getCafe(Item2.get(0));
-        order3.total = order3.getPrice(Item4.get(1)) + order3.getPrice(Item2.get(1));
+        order3.cafe = order3.typeCastCafe(Item4.get(0)) + ", " + order3.typeCastCafe(Item2.get(0));
+        order3.total = order3.typeCastPrice(Item4.get(1)) + order3.typeCastPrice(Item2.get(1));
         order3.ready = false;
 
         Order order4 = new Order();  
         order4.name = "Sam";
-        order4.cafe = "2 " + order4.getCafe(Item4.get(0));
-        order4.total = order4.getPrice(Item4.get(1)) + order4.getPrice(Item4.get(1)) ;
+        order4.cafe = "2 " + order4.typeCastCafe(Item4.get(0));
+        order4.total = order4.typeCastPrice(Item4.get(1)) + order4.typeCastPrice(Item4.get(1)) ;
         order4.ready = false;
 
 
@@ -44,7 +44,7 @@ public class TestOrders {
         // Application Simulations
         // Use this example code to test various orders' updates
         System.out.printf("Name: %s\n", order4.name);
-        System.out.printf("Cafe: %s\n", order4.cafe);
+        System.out.printf("Cafe Ordered: %s\n", order4.cafe);
         System.out.printf("Total: $%s\n", order4.total);
         System.out.printf("Ready: %s\n", order4.ready);
     }
