56315715873782985e1b5cf5d04d81af1021df4c
Lisa <lb.broadhead@gmail.com>
2022-04-04 15:54:22 -0500

    first commit

--- notes/fundamentals/Funda.java
+++ notes/fundamentals/Funda.java
@@ -3,11 +3,22 @@ public class Funda{
         System.out.println("Hello World");
 
         System.out.println("X is eual to 5");
-        addValues();
+        // addValues();
+
+        int[] myArray = {4,5,6,7,8};
+        String[] fruits = {"banans", "Strawberries","Watermelon","Grapes"};
+
+        for(int i = 0; i < fruits.length ; i++){
+            System.out.println(fruits[i]);
+        }
+
+        for(int num : myArray){
+            System.out.println(num);
+        }
+
+        // System.out.println(fruits[0]);
     }
     public void addValues(){
         System.out.println("Hello World");
     }
-    
-    
 }
