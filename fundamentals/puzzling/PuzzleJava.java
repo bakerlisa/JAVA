64f2e876dc5e3d978f4adff3f5d8a3bd6483972f
Lisa <lb.broadhead@gmail.com>
2022-04-03 19:37:05 -0500

    first commit

--- fundamentals/puzzling/PuzzleJava.java
+++ fundamentals/puzzling/PuzzleJava.java
@@ -2,14 +2,13 @@ import java.util.Random;
 import java.util.ArrayList;
 
 class PuzzleJava {
-    public ArrayList<Integer> getTenRolls(){
+    public ArrayList<Integer> getTenRolls(int rollAmount, int max){
         ArrayList<Integer> allRolls = new ArrayList<Integer>();
         
         int i=0;
-        while(i<10){
+        while(i<rollAmount){
             Random rand = new Random();
-            int upperbound = 20;
-            int randInt = rand.nextInt(upperbound); 
+            int randInt = rand.nextInt(max); 
             allRolls.add(randInt);
             i++;
         }
@@ -22,7 +21,7 @@ class PuzzleJava {
     
         // gets random number
         Random rand = new Random();
-        int upperbound = 25;
+        int upperbound = 26;
         int randInt = rand.nextInt(upperbound); 
         
         //sets the radnom character to be returned
@@ -39,8 +38,47 @@ class PuzzleJava {
             password += getRandomLetter();
             i++;
         }
-        System.out.println(password);
 
         return password;
+    }
+
+    public String getNewPasswordSet(int passLength){
+        int i = 0;
+        String passwordSet = new String();
+
+        while(i<passLength){
+            passwordSet += getRandomLetter();
+            i++;
+        }
+
+        return passwordSet;
+    }
+
+    public char[] shuffleArray(char[] passReset){
+        System.out.println(passReset);
+        char[] shuffled = passReset;
+        int i = 0;
+        while(i < passReset.length){
+            Random rand = new Random();
+            int randInt = rand.nextInt(8); 
+
+            //shuffling begins
+            char temp = shuffled[randInt];
+            shuffled[randInt] = shuffled[i];
+            shuffled[i] = temp;
+            
+            i++;
+        }
+        return shuffled;
+    }
+
+    public char[] shuffleArrayTwo(char[] passReset){
+        char[] shuffled = new char[8];
+        int i = 0;
+        while(i < passReset.length){
+            shuffled[i] = getRandomLetter();
+            i++;
+        }
+        return shuffled;
     }
 }
--- fundamentals/puzzling/TestPuzzleJava.java
+++ fundamentals/puzzling/TestPuzzleJava.java
@@ -3,18 +3,34 @@ import java.util.ArrayList;
 public class TestPuzzleJava {
     
 	public static void main(String[] args) {
-		// RANDOM ARRAY OF ROLLS
+		// getTenRolls
 		PuzzleJava generator = new PuzzleJava();
-		ArrayList<Integer> randomRolls = generator.getTenRolls();
-		System.out.println(randomRolls);
+		ArrayList<Integer> randomRolls = generator.getTenRolls(10,20);
+		// System.out.println(randomRolls);
 		
-    	// RANDOM CHARACTER
+    	// getRandomLetter
 		char newLetter = generator.getRandomLetter();
-		System.out.println(newLetter);
+		// System.out.println(newLetter);
 
 		// Passwordgenerator
 		String newPassword = generator.generatePassword();
-		System.out.println(newPassword);
+		// System.out.println(newPassword);
 
+		// getNewPasswordSet
+		int passLen = 10;
+		String setPassword = generator.getNewPasswordSet(passLen);
+		// System.out.println(setPassword);
+
+		// SENEI BONUS: shuffleArray
+		System.out.println();
+		char[] myPass = {'e','w','e','l','r','o','p','x'};
+		char[] shuffledPass = generator.shuffleArray(myPass);
+		System.out.println(shuffledPass);
+
+		
+		// SENEI BONUS: shuffleArray #2
+		char[] myPassTwo = new char[8];
+		char[] shuffledPassTwo = generator.shuffleArrayTwo(myPassTwo);
+		// System.out.println(shuffledPassTwo);
 	}
 }
