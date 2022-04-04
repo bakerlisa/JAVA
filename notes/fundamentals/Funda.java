cda44b436bbd55578fcf67cd3a9cf1e5c4bd4df2
Lisa <lb.broadhead@gmail.com>
2022-04-04 13:15:13 -0500

    first commit

---
+++ notes/fundamentals/Funda.java
@@ -0,0 +1,13 @@
+public class Funda{
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+
+        System.out.println("X is eual to 5");
+        addValues();
+    }
+    public void addValues(){
+        System.out.println("Hello World");
+    }
+    
+    
+}
