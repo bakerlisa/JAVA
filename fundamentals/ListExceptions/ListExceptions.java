d59a9b0cc65ebee8545a79ccd4b5140058b9e054
Lisa <lb.broadhead@gmail.com>
2022-04-04 07:54:35 -0500

    first commit

--- fundamentals/ListExceptions/ListExceptions.java
+++ fundamentals/ListExceptions/ListExceptions.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 public class ListExceptions {
-    
+    public void exceptionsLoop(ArrayList<Object> mylist){
+        for(int i = 0; i < mylist.size(); i++) {
+            try{
+                Integer castedValue = (Integer) mylist.get(i);
+                
+            } catch(Exception e) {
+                System.out.println(e);
+                // back-up plan here.
+            }
+        } 
+    }
 }
--- fundamentals/ListExceptions/TestListExceptions.java
+++ fundamentals/ListExceptions/TestListExceptions.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class TestListExceptions{
     public static void main(String[] args){
-        
+        ArrayList<Object> myList = new ArrayList<Object>();
+
+        myList.add("Lisa");
+        myList.add(31);
+        myList.add(300.50);
+        myList.add("Youi're the best!");
+
+        ListExceptions generator = new ListExceptions();
+
+        generator.exceptionsLoop(myList);
     }
+    
 }
