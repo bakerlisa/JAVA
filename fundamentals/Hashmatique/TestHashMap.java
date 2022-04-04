199da3164ac6e8af4575b85967a42d80b9e123a3
Lisa <lb.broadhead@gmail.com>
2022-04-03 21:58:03 -0500

    first commit

---
+++ fundamentals/Hashmatique/TestHashMap.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class TestHashMap {
+    public static void main(String[] args){
+        HashMap hashTest = new HashMap();
+        HashMap<String, String> userMap = new HashMap<String, String>();
+
+    }
+}
