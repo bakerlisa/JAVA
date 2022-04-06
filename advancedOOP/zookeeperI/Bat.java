612c795ed4f6dd7d15e1a71048ef39033d86f4ff
Lisa <lb.broadhead@gmail.com>
2022-04-05 22:03:06 -0500

    first commit

---
+++ advancedOOP/zookeeperI/Bat.java
@@ -0,0 +1,21 @@
+public class Bat extends Mammal{
+
+    // CONSTRUCTORS
+    public Mammal(){
+        this.engeryLevel = 300;
+    }
+    
+    public void fly(){
+
+    }
+
+    public void eatHumans(){
+
+    }
+
+    public void attackTown(){
+
+    }
+
+
+}
