dd58ae293edfadea3fe231a9d6200413730c333d
Lisa <lb.broadhead@gmail.com>
2022-04-07 09:07:40 -0500

    first commit

--- advancedOOP/abstractArt/TestMuseum.java
+++ advancedOOP/abstractArt/TestMuseum.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 
 public class TestMuseum {
     public static void main(String[] args){
-        
-
         // Painting
         Painting Paint = new Painting("Jackson Polluck","Painting 1","Why we every call this art I've not idea but who cares I'm making millions for it","oil on canvas");
             
@@ -13,7 +11,6 @@ public class TestMuseum {
         // Sculpture
         Sculpture Sculpt = new Sculpture("Mandel Guiver","Thinking man","Man Sitting and thinking","marble");
         Sculpture Sculpt2 = new Sculpture("Reaching for Apples", "Mary Luise", "Rendering of ayoung girll reaching for an apple that's just out of reach", "Metal wires");
-
 
         // Museum
         ArrayList<Object> museum = new ArrayList<Object>();
