0e4cd333216ffca2fc865bf81f7530a0fe98f903
Lisa <lb.broadhead@gmail.com>
2022-04-08 11:43:29 -0500

    first commit

---
+++ springProjects/groupSrpingDemo/src/test/java/com/codingdojo/lessonDemo/GroupSrpingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.lessonDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupSrpingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
