01f0b8b7cce8911b4a70d6ccf04134a19776b2b8
Lisa <lb.broadhead@gmail.com>
2022-04-09 11:58:47 -0500

    first commit

---
+++ springProjects/com.codingdojo.fruityLoops/src/main/java/com/codingdojo/fruityLoops/homeComtroller.java
@@ -0,0 +1,22 @@
+package com.codingdojo.fruityLoops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeComtroller {
+    @RequestMapping("/")
+    public String index(Model model) {
+//        
+//        String name = "Grace Hopper";
+//        String itemName = "Copper wire";
+//        double price = 5.25;
+//        String description = "Metal strips, also an illustration of nanoseconds.";
+//        String vendor = "Little Things Corner Store";
+    
+    	// Your code here! Add values to the view model to be rendered
+    
+        return "index.jsp";
+    }
+}
