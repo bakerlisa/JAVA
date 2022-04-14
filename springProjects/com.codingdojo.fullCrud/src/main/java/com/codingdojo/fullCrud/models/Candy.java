df97a467e67496cd7da15176af286413c5012044
Lisa <lb.broadhead@gmail.com>
2022-04-14 11:46:51 -0500

    commit

--- springProjects/com.codingdojo.fullCrud/src/main/java/com/codingdojo/fullCrud/models/Candy.java
+++ springProjects/com.codingdojo.fullCrud/src/main/java/com/codingdojo/fullCrud/models/Candy.java
@@ -1,5 +1,47 @@
 package com.codingdojo.fullCrud.models;
 
-public class Candy {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="candy")
+public class Candy {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+    @NotNull
+    @Size(min = 5, max = 200, message="Can't be blank")
+    private String title;
+    
+    @NotNull
+    @Size(min = 5, max = 200, message="Description can't be blank")
+    private String description;
+    
+    @NotNull
+    @Size(min = 5, max = 200, message="vendor between 5 - 200 characters")
+    private String vendor;
+    
+    @NotNull(message="amount cannot be blank")
+    @Min(0)
+    private Double amount;
+    
+    
+    // This will not allow the createdAt column to be updated after creation
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
 }
